88d30bf1321e870dee4035dd5496c67ba855ab4e
z3tz3r0 <kittipan.wang@gmail.com>
2025-03-24 09:40:31 +0700

    make change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 export default function App() {
   return (
-    <div className="flex p-6">
-      <h1 className="text-amber-500">React app starter</h1>
+    <div className="min-h-screen flex justify-center bg-blue-950">
+      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
+        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
+          React App Starter
+        </h1>
+        <section className="w-full p-5 bg-amber-100 flex">
+          <ul className="list-inside list-disc flex-1">
+            <span className="font-semibold">Tech Stack:</span>
+            <li>Vite</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Tailwind</li>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }

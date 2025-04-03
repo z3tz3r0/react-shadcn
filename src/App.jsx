25687f363bb092fce22f5ba9aac55a1c7ecfeb14
z3tz3r0 <kittipan.wang@gmail.com>
2025-04-03 11:16:43 +0700

    Add: components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
+import { Button } from "@/components/ui/button";
+import CardTest from "./components/Card";
+
+
 export default function App() {
 
 
     return (
-        <div className="flex justify-center min-h-screen bg-blue-900">
-            <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-                <h1 className="w-full p-6 font-extrabold bg-amber-100">
-                    React App Starter
-                </h1>
-                <section className="flex w-full p-5 bg-amber-100">
-                    <ul className="flex-1 list-disc list-inside">
-                        <span className="font-semibold">Tech Stack:</span>
-                        <li>Vite</li>
-                        <li>React</li>
-                        <li>JavaScript</li>
-                        <li className="p-4 text-center bg-amber-400">Tailwind</li>
-                    </ul>
-                </section>
+        <>
+            <div className='grid h-screen bg-blue-400 place-content-center'>
+
+                <Button variant="outline" size="lg">
+                    test
+                </Button>
+                <CardTest />
             </div>
-        </div>
+        </>
 
     );
 }
